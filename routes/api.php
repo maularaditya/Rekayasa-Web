10e84b724ab3a0f7eb714618cf95fb71bc36f6b7
maularaditya <maularaditya99@gmail.com>
2023-06-26 00:07:35 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\DosenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Controller Mahasiswa
+Route::get('/mahasiswa/index', [MahasiswaController::class,'index_mahasiswa']);
+Route::get('/mahasiswa/read', [MahasiswaController::class,'mahasiswa_read']);
+Route::post('/mahasiswa/create', [MahasiswaController::class,'mahasiswa_create']);
+Route::put('/mahasiswa/update', [MahasiswaController::class, 'mahasiswa_update']);
+Route::delete('/mahasiswa/delete/{nim}', [MahasiswaController::class, 'mahasiswa_destroy']);
+
+//Controller Dosen
+Route::get('/dosen/index', [DosenController::class,'index_dosen']);
+Route::get('/dosen/read', [DosenController::class,'dosen_read']);
+Route::post('/dosen/create', [DosenController::class,'dosen_create']);
+Route::put('/dosen/update', [DosenController::class, 'dosen_update']);
+Route::delete('/dosen/delete/{nidn}', [DosenController::class, 'dosen_destroy']);
